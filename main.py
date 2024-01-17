0df24fd9fd1c1de7e12851423198849be6d03397
Adamkadaban <adamkadaban@gmail.com>
2024-01-17 14:54:03 -0500

    add boilerplate code from sliver-py docs

---
+++ main.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+import os
+import asyncio
+from sliver import SliverClientConfig, SliverClient
+
+CONFIG_DIR = os.path.join(os.path.expanduser("~"), ".sliver-client", "configs")
+DEFAULT_CONFIG = os.path.join(CONFIG_DIR, "default.cfg")
+
+async def main():
+	config = SliverClientConfig.parse_config_file(DEFAULT_CONFIG)
+	client = SliverClient(config)
+	print('[*] Connected to server ...')
+	await client.connect()
+	sessions = await client.sessions()
+	print(f'[*] Sessions: {sessions}')
+	if len(sessions):
+		print(f'[*] Interacting with session {sessions[0].ID}')
+		interact = await client.interact_session(sessions[0].ID)
+		ls = await interact.ls()
+		print('[*] ls: %r' % ls)
+
+if __name__ == '__main__':
+	asyncio.run(main())
