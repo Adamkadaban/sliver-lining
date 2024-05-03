a0833197da7d547e5fd040bedf585bdc730a1a5d
Adamkadaban <adamkadaban@gmail.com>
2024-05-03 17:37:50 -0400

    add exec function to interact TODO: better logging so I don't manually have to print out host info

--- modules/interact/interact.py
+++ modules/interact/interact.py
@@ -3,7 +3,7 @@ class Interact:
 		self.client = client
 
 
-	async def ifconfig(self):
+	async def ifconfig(self, command):
 		sessions = await self.client.sessions()
 
 		for session in sessions:
@@ -17,3 +17,27 @@ class Interact:
 			print(' --- Interfaces ---')
 			for iface in ifconfig_fields:
 				print(f'\t{iface.Name} - {iface.IPAddresses}')
+
+
+	async def exec(self, command):
+		sessions = await self.client.sessions()
+
+		for session in sessions:
+			print(f'[*] Interacting with {session.Username}@{session.Hostname} - {session.RemoteAddress}')
+			interact = await self.client.interact_session(session.ID)
+			if session.OS == "linux":
+				command_args = command.split()
+				exe = command_args.pop(0)
+				execute = await interact.execute(exe, command_args, True)
+			elif session.OS == "windows":
+				# TODO: Need to come up with a better way to decide powershell or cmd
+				# Maybe just use the same logic as linux?
+				execute = await interact.execute("powershell.exe", ["/c", command], True)
+			else:
+				# Assuming MACOS for now. This might break
+				# print(f"OS: {session.OS} not implemented")
+				command_args = command.split()
+				exe = command_args.pop(0)
+				execute = await interact.execute(exe, command_args, True)
+
+			print(execute.Stdout.decode())
