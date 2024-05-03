aa206f7d94977ea05b82de8570cc8a18004dc6c2
Adamkadaban <adamkadaban@gmail.com>
2024-05-03 18:10:18 -0400

    add function headers in logger.py to be used in logging later needs to have context passed in about session

---
+++ src/logger/logger.py
@@ -0,0 +1,16 @@
+class Logger:
+	def __init__(self, logging_level):
+		self.logging_level = logging_level
+
+
+	def warning(self):
+		pass
+
+	def success(self):
+		pass
+
+	def failure(self):
+		pass
+
+	def debug(self):
+		pass
